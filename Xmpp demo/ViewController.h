ffaa4d1d325eeaec060c4c1a89740d091e4826c9
maomao <maomao@maomaodeMacBook-Pro.local>
2015-07-14 17:28:49 +0800

    Initial Commit

---
+++ Xmpp demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Xmpp demo
+//
+//  Created by maomao on 15/7/14.
+//  Copyright (c) 2015年 maomao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
